4ea39a89de4cf34032c7fee4293b2ce96ae82b47
Gabriela Burgel <gabiiburgel@gmail.com>
2025-11-25 03:02:04 -0300

    colisoes ok

--- src/Gerenciador/GerenciadorColisoes.cpp
+++ src/Gerenciador/GerenciadorColisoes.cpp
@@ -42,47 +42,64 @@ namespace Gerenciador {
     }
 
     sf::Vector2f GerenciadorColisoes::calculaNormal(const sf::Vector2f& vertice1, const sf::Vector2f& vertice2) {
+        // recebe dois vértices e cria um vetor perpendicular
         sf::Vector2f normal;
         float modulo;
 
-        normal.x = - (vertice1.y - vertice2.y);
-        normal.y = vertice1.x - vertice2.x;
-        modulo = sqrt(normal.x * normal.x + normal.y * normal.y);
+        normal.x = - (vertice1.y - vertice2.y); // vetor perpendicular
+        normal.y = vertice1.x - vertice2.x;     // vetor perpendicular
+        modulo = sqrt(normal.x * normal.x + normal.y * normal.y); // comprimento do vetor
+        // divide as normais pelo modulo pra tornar elas unitárias:
         normal.x = normal.x / modulo;
         normal.y = normal.y / modulo;
 
-        return normal;
+        return normal; // extrai 4 normais: 2 do corpo 1 e 2 do corpo 2
     }
 
     vector<pair<float, float>> GerenciadorColisoes::calculaProjecao(const vector<sf::Vector2f> &vertices, const vector<sf::Vector2f> &normal) {
+        // para cada eixo normal ele calcula o menor e o maior valor da projeção
+
         vector<pair<float,float>> projecoes = {{9999999.0f, -9999999.0f}, {9999999.0f, -9999999.0f}, {9999999.0f, -9999999.0f}, {9999999.0f, -9999999.0f}};
+        // projecoes guarda um par <min, max> - inicia com valores extremos
         float resultado;
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
+
+        for (int i = 0; i < 4; i++) {      //loop sobre as normais
+            for (int j = 0; j < 4; j++) {  // loop sobre os vértices
+
                 resultado = vertices[j].x * normal[i].x + vertices[j].y * normal[i].y;
+                // resultado eh a projeção (produto escalar) de cada vértice na normal
+
+                // atualiza o minimo e o maximo  da projeção para aquela normal:
                 if (projecoes[i].first > resultado)
                     projecoes[i].first = resultado;
                 if (projecoes[i].second < resultado)
                     projecoes[i].second = resultado;
             }
         }
-        return projecoes;
+        return projecoes; // retorna o vetor de projeções
     }
 
     sf::Vector2f GerenciadorColisoes::calculaSobreposicao(vector<sf::Vector2f> coordCorpo1, vector<sf::Vector2f> coordCorpo2) {
+        // para cada normal, calcula quanto cada corpo invadiu o outro
+
         vector<sf::Vector2f> normais;
+
         for (int i = 0; i < 2; i++) {
+            // pega duas arestas do corpo, calcula suas normais e coloca no vetor de normais criado
             normais.push_back(calculaNormal(coordCorpo1[i], coordCorpo1[i + 1]));
             normais.push_back(calculaNormal(coordCorpo2[i], coordCorpo2[i + 1]));
         }
-
+        // calcula projeção dos dois corpos em todas as normais
         vector<pair<float, float>> projecaoC1 = calculaProjecao(coordCorpo1, normais);
         vector<pair<float, float>> projecaoC2 = calculaProjecao(coordCorpo2, normais);
 
         float sobreposicao = 999999.0f;
-        sf::Vector2f deltaSobreposicao;
+        sf::Vector2f deltaSobreposicao; // vetor de correção da posição
 
         for (int i = 0; i < 4; i++) {
+            //para cada normal calcula duas possiveis sobreposições
+            // se algum for menor que a atual, atualiza a sobreposição e atualiza deltaSobreposição na direção da normal
+
             if (sobreposicao > projecaoC1[i].second - projecaoC2[i].first) {
                 sobreposicao = projecaoC1[i].second - projecaoC2[i].first;
                 deltaSobreposicao.x = - normais[i].x * sobreposicao;
@@ -95,15 +112,18 @@ namespace Gerenciador {
             }
         }
 
-        if (sobreposicao <= 0.0f) {
+        if (sobreposicao <= 0.0f) {     // nao houve colisão
             deltaSobreposicao.x = 0.0f;
             deltaSobreposicao.y = 0.0f;
         }
-        return deltaSobreposicao;
+
+        return deltaSobreposicao; //retorna o vetor de correção
     }
 
 
     sf::Vector2f GerenciadorColisoes::verificarColisao(Entidade::Entidade* corpo1, Entidade::Entidade* corpo2) {
+        // pega a coordenada dos vértices e passa prara calculaSobreposição(), retornando o vetor de correção
+
         vector<sf::Vector2f> coordCorpo1 = corpo1->getCoordenadas();
         vector<sf::Vector2f> coordCorpo2 = corpo2->getCoordenadas();
 
@@ -112,8 +132,12 @@ namespace Gerenciador {
 
     void GerenciadorColisoes::corrigirColisao(Entidade::Personagem::Personagem* pPersonagem, sf::Vector2f colisao) {
         sf::Vector2f deslocamento = pPersonagem->getDeslocamento();
-        if (colisao.y < 0.0f) {
+        // recebe um personagem e um vetor de colisão - pega o deslocamento atual do personagem
+
+        if (colisao.y < 0.0f) { // se o personagem esta por cima de algo
             pPersonagem->estaNoChao(true);
+
+            // se estava caindo ou bateu no chão, para a queda
             if (colisao.x != 0.0f) {
                 float tg;
                 tg = colisao.x / colisao.y;
@@ -123,26 +147,31 @@ namespace Gerenciador {
             } else if (deslocamento.y > 0.0f) {
                 deslocamento.y = 0.0f;
             }
-            if (colisao.y < -30.0f)
+            if (colisao.y < -30.0f) // colisão muito forte, toma dano
                 pPersonagem->tomarDano(100);
-        } else if (colisao.y > 0.0f) {
+
+        } else if (colisao.y > 0.0f) { // bateu no teto, colisão inverte
             deslocamento.y = deslocamento.y * -1;
         }
-        pPersonagem->setDeslocamento(deslocamento);
-        colisao.x += pPersonagem->getPosicao().x;
+
+        // atualiza a posição do jogador:
+        pPersonagem->setDeslocamento(deslocamento);  // pega o vetor de correção
+        colisao.x += pPersonagem->getPosicao().x;    // soma a posição
         colisao.y += pPersonagem->getPosicao().y;
-        pPersonagem->atualizarPos(colisao);
+        pPersonagem->atualizarPos(colisao);          // move o jogador pra fora da colisão
     }
 
     void GerenciadorColisoes::tratarColisoesJogsObstacs() {
-        sf::Vector2f colisao;
-        if (pJogador2) {
-            colisao = verificarColisao(pJogador1, pJogador2);
+        sf::Vector2f colisao; // cria um vetor pra guardar sobreposição entre dois corpos
+
+        if (pJogador2) { // se tem outro jogador
+            colisao = verificarColisao(pJogador1, pJogador2); //calcula se os dois se sobrepõem
+            // cada um desloca metade da correção de colisão e eles se empurram
             if (colisao.x != 0.0f || colisao.y != 0.0f) {
                 corrigirColisao(pJogador1, {colisao.x / 2, colisao.y / 2});
                 corrigirColisao(pJogador2, {colisao.x * -0.5f, colisao.y * -0.5f});
                 if (colisao.y > 0.0f) {
-                    pJogador2->pular(0.5f);
+                    pJogador2->pular(0.5f); // se jogador 2 tiver abaixo entao pula - quica um no outro
                 } else if (colisao.y < 0.0f) {
                     pJogador1->pular(0.5f);
                 }
@@ -152,10 +181,11 @@ namespace Gerenciador {
         for (list<Entidade::Obstaculo::Obstaculo*>::const_iterator it = LOs.begin(); it != LOs.end(); it++) {
             colisao = verificarColisao(pJogador1, *it);
             if (colisao.x != 0.0f || colisao.y != 0.0f) {
-                corrigirColisao(pJogador1, colisao);
+                corrigirColisao(pJogador1, colisao); // se houver colisão com o obstaculo, corrige
                 if (colisao.y < 0.0f)
-                    (*it)->obstaculizar(pJogador1);
+                    (*it)->obstaculizar(pJogador1); // se bater no obstaculo por cima, chama obstaculizar()
             }
+            // se houver jogador 2, faz o mesmo pra ele
             if (pJogador2) {
                 colisao = verificarColisao(pJogador2, *it);
                 if (colisao.x != 0.0f || colisao.y != 0.0f) {
@@ -167,6 +197,8 @@ namespace Gerenciador {
         }
 
         for (list<Entidade::Chao*>::const_iterator it = LCs.begin(); it != LCs.end(); it++) {
+            // verifica se há colisão com o chão e chama corrigirColisão
+
             colisao = verificarColisao(pJogador1, *it);
             if (colisao.x != 0.0f || colisao.y != 0.0f) {
                 corrigirColisao(pJogador1, colisao);
@@ -186,12 +218,14 @@ namespace Gerenciador {
 
     void GerenciadorColisoes::tratarColisoesJogsInimigs() {
         sf::Vector2f colisao;
+
+            // percorre todos os inimigos da lista:
             for (vector<Entidade::Personagem::Inimigo::Inimigo*>::const_iterator it = LIs.begin(); it != LIs.end(); it++) {
-                if ((*it)->estaAtivo()) {
+                if ((*it)->estaAtivo()) { // só processa se o inimigo está ativo
                     colisao = verificarColisao(pJogador1, *it);
-                    if (colisao.x != 0.0f || colisao.y != 0.0f) {
-                        corrigirColisao(pJogador1, colisao);
-                        pJogador1->colidir(*it, colisao);
+                    if (colisao.x != 0.0f || colisao.y != 0.0f) { // verifica se tem colisão
+                        corrigirColisao(pJogador1, colisao);      // chama corrigirColisao() pra ajustar a posição
+                        pJogador1->colidir(*it, colisao);         // chama função colidir
                     }
                     if (pJogador2) {
                         colisao = verificarColisao(pJogador2, *it);
@@ -206,6 +240,8 @@ namespace Gerenciador {
 
     void GerenciadorColisoes::tratarColisoesInimigsObstacs() {
         sf::Vector2f colisao;
+
+        // percorre as listas de inimigos e de obstaculo e se houver sobreposição ajusta a posição de inimigo
         for (vector<Entidade::Personagem::Inimigo::Inimigo*>::const_iterator it = LIs.begin(); it != LIs.end(); it++) {
             for (list<Entidade::Obstaculo::Obstaculo*>::const_iterator it2 = LOs.begin(); it2 != LOs.end(); it2++) {
                 colisao = verificarColisao(*it, *it2);
@@ -225,6 +261,8 @@ namespace Gerenciador {
 
     void GerenciadorColisoes::tratarColisoesEventos() {
         sf::Vector2f colisao;
+
+        // percorre a lista de peixes e considera os peixes ativos, se tiver peixe, desaativa ele e coleta
         for (vector<Entidade::Itens::Peixe*>::const_iterator it = LPxs.begin(); it != LPxs.end(); it++) {
             if ((*it)->estaAtivo()) {
                 colisao = verificarColisao(*it, pJogador1);
@@ -245,6 +283,8 @@ namespace Gerenciador {
 
     void GerenciadorColisoes::tratarColisoesProjeteis() {
         sf::Vector2f colisao;
+
+        // percorre a lista de projeteis, se colidir destiva ele e o jogador leva dano via colidir()
         for (set<Entidade::Itens::Projetil*>::const_iterator it = LPs.begin(); it != LPs.end(); it++) {
             if ((*it)->estaAtivo()) {
                 colisao = verificarColisao(pJogador1, *it);
